90345dd5db6e36a9e471b18c7f60ed4359dad6e8
Twileon <shenhantang@126.com>
2020-06-05 00:31:31 +0800

    xxx

---
+++ enhance/FAVisualizer/data.js
@@ -0,0 +1,91 @@
+window._seulex_shape = 'circle'; let data = {
+  "nodes": [
+    {
+      "key": "0",
+      "label": "0",
+      "color": "#46A3FF"
+    },
+    {
+      "key": "1",
+      "label": "1",
+      "color": "#FFFFFF"
+    },
+    {
+      "key": "2",
+      "label": "2",
+      "color": "#FFFFFF"
+    },
+    {
+      "key": "3",
+      "label": "3",
+      "color": "#00DB00"
+    },
+    {
+      "key": "4",
+      "label": "4",
+      "color": "#00DB00"
+    }
+  ],
+  "edges": [
+    {
+      "source": "0",
+      "target": "1",
+      "name": "0_0",
+      "label": "a"
+    },
+    {
+      "source": "0",
+      "target": "2",
+      "name": "0_1",
+      "label": "b"
+    },
+    {
+      "source": "1",
+      "target": "3",
+      "name": "1_0",
+      "label": "a"
+    },
+    {
+      "source": "1",
+      "target": "4",
+      "name": "1_1",
+      "label": "b"
+    },
+    {
+      "source": "2",
+      "target": "1",
+      "name": "2_0",
+      "label": "a"
+    },
+    {
+      "source": "2",
+      "target": "2",
+      "name": "2_1",
+      "label": "b"
+    },
+    {
+      "source": "3",
+      "target": "3",
+      "name": "3_0",
+      "label": "a"
+    },
+    {
+      "source": "3",
+      "target": "4",
+      "name": "3_1",
+      "label": "b"
+    },
+    {
+      "source": "4",
+      "target": "1",
+      "name": "4_0",
+      "label": "a"
+    },
+    {
+      "source": "4",
+      "target": "2",
+      "name": "4_1",
+      "label": "b"
+    }
+  ]
+}
